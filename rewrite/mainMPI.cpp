d7a2d702e332f8138a6b108e2dbde6b51261c7e7
Jesaja <jesaja.weintritt@stud.eah-jena.de>
2026-08-16 12:47:53 +0200

    lic

--- rewrite/mainMPI.cpp
+++ rewrite/mainMPI.cpp
@@ -1,3 +1,12 @@
+/*
+Copyright (C) 2026 Jesaja Weintritt (jesaja.weintritt@stud.eah-jena.de) and 2012 Mark Boots (mark.boots@usask.ca).
+
+This program was originally implemented as a part of the Parallel Efficiency of Gratings project PEG and got reworked in 2026. PEG is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License, version 3, as published by the Free Software Foundation.
+See <http://www.gnu.org/licenses/> for details.
+
+This reworked version contains substantial modifications by Jesaja Weintritt (2026) and has not been independently verified against the original. It is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; use at your own risk and verify results independently.
+*/
+
 #include "PEG.h"
 #include "MainSupport.h"
 
